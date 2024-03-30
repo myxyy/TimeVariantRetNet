4409892e3bb55b86c92c228999fd9fb3a0230248
myxyy <m.myxomatosis@gmail.com>
2024-03-30 22:26:08 +0900

    行列化

--- src/model/sioconv.py
+++ src/model/sioconv.py
@@ -19,16 +19,17 @@ class FFN(nn.Module):
         return x
 
 class SioConvLayer(nn.Module):
-    def __init__(self, dim: int, num_head: int, dtype):
+    def __init__(self, dim: int, inner_dim: int, dtype):
         super().__init__()
         self.dim = dim
-        self.num_head = num_head
-        self.linear_a = nn.Linear(dim, num_head*2, bias=False)
-        self.linear_x = nn.Linear(dim, dim*2, bias=False)
-        self.linear_out = nn.Linear(dim, dim, bias=False)
+        self.inner_dim = inner_dim 
+        self.linear_a = nn.Linear(dim, inner_dim*2, bias=False)
+        self.linear_x = nn.Linear(dim, inner_dim*2, bias=False)
+        self.mat_v = nn.Parameter(torch.randn(inner_dim, inner_dim, dtype=torch.cfloat))
+        self.linear_out = nn.Linear(inner_dim, dim, bias=False)
 
         self.last_hidden = None
-        self.last_hidden_init = nn.Parameter(torch.randn(dim, dtype=torch.cfloat))
+        self.last_hidden_init = nn.Parameter(torch.randn(inner_dim, dtype=torch.cfloat))
         self.is_refresh = True
 
     #(batch, len, dim) -> (batch, len, dim)
@@ -36,42 +37,45 @@ class SioConvLayer(nn.Module):
         batch = x.shape[0]
         len = x.shape[1]
         dim = self.dim
-        num_head = self.num_head
+        inner_dim = self.inner_dim
         dtype = x.dtype
 
         if self.last_hidden is None:
-            self.last_hidden = self.last_hidden_init.unsqueeze(0).expand(batch, dim)
+            self.last_hidden = self.last_hidden_init.unsqueeze(0).expand(batch, inner_dim)
         else:
             self.last_hidden = self.last_hidden.detach()
 
-        a = torch.view_as_complex(self.linear_a(x.float()).view(batch, len, num_head, 2)) # (batch, len, num_head)
+        a = torch.view_as_complex(self.linear_a(x.float()).view(batch, len, inner_dim, 2)) # (batch, len, inner_dim)
         a_sqr_mag = a.real * a.real + a.imag * a.imag
-        a = a * torch.rsqrt(a_sqr_mag) * torch.exp(-torch.tanh(a_sqr_mag))
+        a = a * torch.rsqrt(a_sqr_mag) * torch.sigmoid(torch.log(a_sqr_mag))
 
-        x = torch.view_as_complex(self.linear_x(x.float()).view(batch, len, dim, 2))
+        x = torch.view_as_complex(self.linear_x(x.float()).view(batch, len, inner_dim, 2))
         x_sqr_mag = x.real * x.real + x.imag * x.imag
         x = x * torch.rsqrt(x_sqr_mag) * x_sqr_mag
-        x = x.view(batch, len, num_head, dim//num_head)
+        x = x.view(batch, len, inner_dim)
 
         if len == 1:
-            h = (a.unsqueeze(3) * self.last_hidden.view(batch, num_head, dim//num_head).unsqueeze(1) + x).view(batch, len, dim)
+            vav_inv = torch.matmul(self.mat_v.unsqueeze(0).unsqueeze(1) * a.unsqueeze(3), torch.inverse(self.mat_v))
+            h = (torch.matmul(vav_inv, self.last_hidden.unsqueeze(1).unsqueeze(3)).squeeze(3) + x)
             if self.is_refresh:
                 self.last_hidden = h[:,-1,:]
             return self.linear_out(h.real).to(dtype)
 
         a_ln = torch.log(a)
-        a_ln_tri = a_ln.transpose(2,1).unsqueeze(2).expand(batch, num_head, len, len).triu() # (batch, num_head, len, len)
+        a_ln_tri = a_ln.transpose(2,1).unsqueeze(2).expand(batch, inner_dim, len, len).triu() # (batch, inner_dim, len, len)
         a_ln_tri_fft = torch.fft.fft(a_ln_tri, n=len*2, dim=3)
         ones_fft = torch.fft.fft(torch.ones(len, len, device=x.device), n=len*2, dim=1)
-        a_ln_tri_conv = torch.fft.ifft(a_ln_tri_fft * ones_fft.unsqueeze(0).unsqueeze(1)).narrow(3,0,len) # (batch, num_head, len, len)
-        c = torch.exp(a_ln_tri_conv).triu(diagonal=-1) # (batch, num_head, len, len)
+        a_ln_tri_conv = torch.fft.ifft(a_ln_tri_fft * ones_fft.unsqueeze(0).unsqueeze(1)).narrow(3,0,len) # (batch, inner_dim, len, len)
+        c = torch.exp(a_ln_tri_conv).triu(diagonal=-1) # (batch, inner_dim, len, len)
+        mat_v_unsqueeze = self.mat_v.unsqueeze(0).unsqueeze(1).unsqueeze(2)
+        c = torch.matmul(mat_v_unsqueeze * c.permute(0,2,3,1).unsqueeze(3), torch.inverse(mat_v_unsqueeze)) # (batch, len, len, inner_dim, inner_dim)
 
-        x = x.permute(0,2,3,1) # (batch, num_head, dim//num_head, len)
-        x_roll = x.roll(1, dims=3) # (batch, num_head, dim//num_head, len)
-        x_roll[:,:,:,0] = self.last_hidden.view(batch, num_head, dim//num_head)
-        h = torch.matmul(c.transpose(3,2), x_roll.transpose(3,2)).transpose(3,2)  # (batch, num_head, dim//num_head, len)
-        h[:,:,:,-1] += x[:,:,:,-1]
-        h = h.permute(0,3,1,2).reshape(batch, len, dim) # (batch, len, num_head, dim//num_head)
+        x = x.transpose(2,1) # (batch, inner_dim, len)
+        x_roll = x.roll(1, dims=2) # (batch, inner_dim, len)
+        x_roll[:,:,0] = self.last_hidden
+        h = torch.einsum("blmno,bol->bmn", c, x_roll).transpose(2,1) # (btach, inner_dim, len)
+        h[:,:,-1] += x[:,:,-1]
+        h = h.transpose(2,1).reshape(batch, len, inner_dim) # (batch, len, inner_dim)
         if self.is_refresh:
             self.last_hidden = h[:,-1,:]
         return self.linear_out(h.real).to(dtype)
@@ -83,12 +87,12 @@ class SioConvLayer(nn.Module):
         self.is_refresh = is_refresh
 
 class SioConvBlock(nn.Module):
-    def __init__(self, dim: int, dim_ff_hidden: int, num_head: int, dropout: float, dtype):
+    def __init__(self, dim: int, dim_ff_hidden: int, inner_dim: int, dropout: float, dtype):
         super().__init__()
         self.dtype = dtype 
 
         self.layer_norm_sc_in = nn.LayerNorm(dim, elementwise_affine=True, bias=True, dtype=dtype)
-        self.sioconv = SioConvLayer(dim, num_head, dtype)
+        self.sioconv = SioConvLayer(dim, inner_dim, dtype)
 
         self.layer_norm_ffn_sc_in = nn.LayerNorm(dim, elementwise_affine=True, bias=True, dtype=dtype)
         self.ffn_sc = FFN(dim, dim_ff_hidden, dtype)
@@ -122,7 +126,7 @@ class SioConv(nn.Module):
         depth: int,
         dim: int,
         dim_ff_hidden: int,
-        num_head: int,
+        inner_dim: int,
         dropout: float,
         vocab_size: int,
         devices,
@@ -138,7 +142,7 @@ class SioConv(nn.Module):
         self.token_out = nn.Linear(dim, vocab_size, device=devices[-1], dtype=dtype)
         nn.init.normal_(self.token_out.weight, std=dim**-0.5)
         nn.init.constant_(self.token_out.bias, 0)
-        self.block_list = nn.ModuleList([SioConvBlock(dim, dim_ff_hidden, num_head, dropout, dtype) for _ in range(depth)])
+        self.block_list = nn.ModuleList([SioConvBlock(dim, dim_ff_hidden, inner_dim, dropout, dtype) for _ in range(depth)])
         self.layer_norm_last = nn.LayerNorm(dim, elementwise_affine=True, bias=True, device=devices[-1], dtype=dtype)
 
         self.num_parameters_token_in = sum(p.numel() for p in self.token_in.parameters())
